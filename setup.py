0dcc03d9c61245d3deafcfc6518f80f22989ea99
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-07-24 22:58:47 +0800

    alvistack/2.9.1
        git clean -xdf
        tar zcvf ../python-soupsieve_2.9.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-soupsieve.spec ../python-soupsieve_2.9.1-1.spec
        cp ../python*-soupsieve*2.9.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/facelessuser-soupsieve-2.9.1/
        rm -rf ../python*-soupsieve*2.9.1*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='soupsieve',
+    version='2.9.1',
+    description='A modern CSS selector implementation for Beautiful Soup.',
+    author_email='Isaac Muse <Isaac.Muse@gmail.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
+    packages=[
+        'soupsieve',
+    ],
+)
